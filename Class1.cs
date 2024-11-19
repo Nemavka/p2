2569c47bb1eb5b6b18235fd58ee7d2e8fcf578bf
Nemavka <74415985+Nemavka@users.noreply.github.com>
2024-11-19 21:21:38 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace USQLCSharpProject1_for_practical_work_2
+{
+    public class ComplexNumber
+    {
+        double RealPart = 0;
+        double ImaginaryPart = 0;
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            RealPart = real;
+            ImaginaryPart = imaginary;
+        }
+
+        public override string ToString()
+        {
+            return $"{RealPart} + {ImaginaryPart}i";
+        }
+
+        public static ComplexNumber operator +(ComplexNumber n1, ComplexNumber n2)
+        {
+            double RealSum = n1.RealPart + n2.RealPart;
+            double ImaginarySum = n1.ImaginaryPart + n2.ImaginaryPart;
+            return new ComplexNumber(RealSum, ImaginarySum);
+        }
+
+        public static ComplexNumber operator +(ComplexNumber n, double real)
+        {
+            return new ComplexNumber(n.RealPart + real, n.ImaginaryPart);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber n1, ComplexNumber n2)
+        {
+            double RealSum = n1.RealPart - n2.RealPart;
+            double ImaginarySum = n1.ImaginaryPart - n2.ImaginaryPart;
+            return new ComplexNumber(RealSum, ImaginarySum);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber n, double real)
+        {
+            return new ComplexNumber(n.RealPart - real, n.ImaginaryPart);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber n1, ComplexNumber n2)
+        {
+            double RealSum = n1.RealPart * n2.RealPart - n1.ImaginaryPart * n2.ImaginaryPart;
+            double ImaginarySum = n1.RealPart * n2.ImaginaryPart + n1.ImaginaryPart * n2.RealPart;
+            return new ComplexNumber(RealSum, ImaginarySum);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber n, double real)
+        {
+            return new ComplexNumber(n.RealPart * real, n.ImaginaryPart);
+        }
+
+        public static bool operator true(ComplexNumber n)
+        {
+            return n.RealPart != 0 || n.ImaginaryPart != 0;
+        }
+
+        public static bool operator false(ComplexNumber n)
+        {
+            return n.RealPart == 0 && n.ImaginaryPart == 0;
+        }
+
+        public static implicit operator int(ComplexNumber n)
+        {
+            return (int)n.RealPart;
+        }
+
+        public static explicit operator double(ComplexNumber n)
+        {
+            return n.RealPart;
+        }
+
+        public static bool operator ==(ComplexNumber n1, ComplexNumber n2)
+        {
+            if (ReferenceEquals(n1, null) &&  ReferenceEquals(n2, null)) return true;
+            if (ReferenceEquals(n1, null) || ReferenceEquals(n2, null)) return false;
+            return n1.RealPart == n2.RealPart && n1.ImaginaryPart == n2.ImaginaryPart;
+        }
+
+        public static bool operator !=(ComplexNumber n1, ComplexNumber n2)
+        {
+            return !(n1 == n2);
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        public static bool operator >(ComplexNumber n1, ComplexNumber n2)
+        {
+            return n1.Magnitude() > n2.Magnitude();
+        }
+
+        public static bool operator >=(ComplexNumber n1, ComplexNumber n2)
+        {
+            return n1.Magnitude() >= n2.Magnitude();
+        }
+        public static bool operator <(ComplexNumber n1, ComplexNumber n2)
+        {
+            return n1.Magnitude() < n2.Magnitude();
+        }
+        public static bool operator <=(ComplexNumber n1, ComplexNumber n2)
+        {
+            return n1.Magnitude() <= n2.Magnitude();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using USQLCSharpProject1_for_practical_work_2;
+
+namespace PracticalWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber n1 = new ComplexNumber(1.34, 2.77);
+            ComplexNumber n2 = new ComplexNumber(-3.94, 9.01);
+
+            Console.WriteLine($"{n1} + {n2} = {n1 + n2}");
+            Console.WriteLine($"{n1} - {n2} = {n1 - n2}");
+            Console.WriteLine($"{n1} * {n2} = {n1 * n2}");
+
+            double real = -33.31;
+
+            Console.WriteLine($"{n1} + {real} = {n1 + real}");
+            Console.WriteLine($"{n1} - {real} = {n1 - real}");
+            Console.WriteLine($"{n1} * {real} = {n1 * real}");
+
+            if (n1 == n2) Console.WriteLine($"{n1} is equal {n2}");
+            else Console.WriteLine($"{n1} isn't equal {n2}");
+
+            ComplexNumber n3 = new ComplexNumber(1.34, 2.77);
+
+            if (n1 == n3) Console.WriteLine($"{n1} is equal {n3}");
+            else Console.WriteLine($"{n1} isn't equal {n3}");
+
+            Console.WriteLine($"{n1} > {n2}? {n1 > n2}");
+            Console.WriteLine($"{n1} >= {n3}? {n1 >= n3}");
+            Console.WriteLine($"{n1} < {n2}? {n1 < n2}");
+            Console.WriteLine($"{n1} <= {n3}? {n1 <= n3}");
+
+            if (n1) Console.WriteLine($"{n1} is true");
+            else Console.WriteLine($"{n1} is false");
+
+            ComplexNumber n4 = new ComplexNumber(0, 0);
+
+            if (n4) Console.WriteLine($"{n4} is true");
+            else Console.WriteLine($"{n4} is false");
+
+            int realpart = n2;
+            double realpart2 = (double)n2;
+
+            Console.WriteLine($"implicit int {n2} is {realpart}, explicit double {n2} is {realpart2}");
+        }
+    }
+}
